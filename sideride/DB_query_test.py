e7a47d6847dd7c0eaa1a2f58233230276c1d53ce
akuroodi <akuroodi@gmail.com>
2021-11-11 16:11:26 -0800

    updated amplify

--- sideride/DB_query_test.py
+++ sideride/DB_query_test.py
@@ -9,24 +9,44 @@ config = {
   'user': 'SideRideProject',
   'password': 'SideRideProject130*',
   'host': 'database-side-ride-project.ch9vjbvoh8tk.us-east-2.rds.amazonaws.com',
-  'database': 'SideRideSchema4',
+  'database': 'SideRideSchema',
   'raise_on_warnings': True
 }
 
 try:
     connection = ms.connect(**config)
-    print(connection.is_connected())
+    connection.get_warnings = True
     cursor = connection.cursor()
 except ms.Error as e:
     print(e.msg)
-    print(connection.is_connected())
+    #print(connection.is_connected())       use to check if connection still alive 
     exit()
 
-# Once we have established a connection, do whatever you want 
+# Once we have established a connection, do whatever you want to the table
+insert_stmt = (
+  "INSERT INTO Rides (driver_id, start, stop, date) "
+  "VALUES (%s, %s, %s, %s)"
+)
+data = (5, 'Los Angeles', 'Tijuana', '2012-05-12')
 
-query = ("SELECT * FROM LoginInformation WHERE Password = 'TestPassword' ")
-x = cursor.execute(query)
-if cursor: print("woo!")
+table = "LoginInformation"
+
+query_stmt = (
+    "DELETE FROM Rides WHERE stop = %s"
+)
+
+data2 = ('Tijuana',)
+# query = ("SELECT * FROM " +table+ " WHERE Password = 'TestPassword1f' ")
+
+# This catches improper queries 
+try:
+    cursor.execute(query_stmt,data2)
+    connection.commit()
+except:
+    cursor.fetchwarnings()
+
+a = cursor.fetchall()
+print(f"Cursor had: {a}")           # this catches valid queries that return empty set 
 
 for user, pw in cursor:
     print(f"We have user: {user} with password: {pw}")      # basic test to print to console 
