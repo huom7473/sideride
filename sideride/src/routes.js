b3f88ec72b401ef8867e15c4ba105ac3f3e2a47f
tyganzeng <zengtygan@gmail.com>
2021-11-21 23:07:09 -0800

    Fixed issues with new login system, deleted login and createact

--- sideride/src/routes.js
+++ sideride/src/routes.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 
-import App from "./App";
 import SearchPage from "./SearchPage"
-import LoginPage from "./LoginPage"
 import NotFoundPage from "./NotFoundPage"
 import ResultsPage from "./ResultsPage"
 import CreateRidePage from "./CreateRide"
-import CreateAccountPage from "./CreateAccount"
 
 export default (
     <Switch>
-        <Route exact path="/" component={LoginPage}/>
-        <Route path="/login" component={LoginPage}/>
-        <Route path="/search" component={SearchPage}/>
-        <Route path="/results" component={ResultsPage}/>
-        <Route path="/createride" component={CreateRidePage}/>
-        <Route path="/create" component={CreateAccountPage}/> 
-        <Route component={NotFoundPage}/>
+        <Route exact path="/" component={SearchPage} />
+        <Route path="/results" component={ResultsPage} />
+        <Route path="/createride" component={CreateRidePage} />
+        <Route component={NotFoundPage} />
     </Switch>
 
 
