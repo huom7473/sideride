955beb072640d0c2e86ffb9c5c5e48b643a74e3c
Austin Keil <austin.c.keil@gmail.com>
2021-11-27 13:51:22 -0800

    updated selection tests in searchpage.test.js

--- sideride/src/test/searchpage.test.js
+++ sideride/src/test/searchpage.test.js
@@ -1,4 +1,4 @@
-import { Selection } from "../SearchPage"
+import Selection from "../Selection"
 import { configure, mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
@@ -7,9 +7,18 @@ configure({ adapter: new Adapter() });      // for enzyme methods like mount
 
 const wrapper = mount(<Selection />);
 const instance = wrapper.instance();
-const dummy_event = {
-    target: { value: 'test-value' }
-};
+
+const dummy_from = {
+  lat: 12,
+  lng: 34,
+  description: '123 from street'
+}
+
+const dummy_to = {
+  lat: 56,
+  lng: 78,
+  description: '123 to street'
+}
 
 test('Search page: initialization', () => {
     expect(instance.state.from).toEqual("");
@@ -18,17 +27,16 @@ test('Search page: initialization', () => {
   });
 
 
+test('Search page: _handleFromLatLong correctly sets from and fromCoord', () => {
+    instance._handleFromLatLong(dummy_from);
+    expect(instance.state.from).toBe(dummy_from.description);
+    expect(instance.state.fromCoord.lat).toBe(dummy_from.lat);
+    expect(instance.state.fromCoord.lng).toBe(dummy_from.lng);
+});
 
-test('Search page: update states (to,from,date) ', () => {
-    instance.handleChange(dummy_event);
-    expect(instance.state.from).toBe(dummy_event.target.value);
-
-    instance.handleToChange(dummy_event);
-    expect(instance.state.to).toBe(dummy_event.target.value);
-
-
-    instance.handleDateChange(dummy_event);
-    expect(instance.state.date).toBe(dummy_event.target.value);
-
-    });
-
+test('Search page: _handleToLatLong correctly sets to and toCoord', () => {
+  instance._handleFromLatLong(dummy_from);
+  expect(instance.state.from).toBe(dummy_from.description);
+  expect(instance.state.fromCoord.lat).toBe(dummy_from.lat);
+  expect(instance.state.fromCoord.lng).toBe(dummy_from.lng);
+});
