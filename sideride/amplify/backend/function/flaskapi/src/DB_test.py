d93df1b5854422d6f31cdd519d645ce3428daec6
akuroodi <akuroodi@gmail.com>
2021-11-28 20:37:25 -0800

    anthony - added pytest install for backend testing

--- sideride/amplify/backend/function/flaskapi/src/DB_test.py
+++ sideride/amplify/backend/function/flaskapi/src/DB_test.py
@@ -1,66 +1,117 @@
-import unittest
-from DB_manager import *
+import pytest
+import DB_manager as dm
+import Ride_manager as rm
+import mysql.connector as ms
 
-test_configs = [
-    #user is incorrect, expect fail
+#run test with: pytest .\src\ --cov=. --cov-branch
+#from the flaskapi folder
+
+default_config = {
+    'user': 'SideRideProject',
+    'password': 'SideRideProject130*',
+    'host': 'database-side-ride-project.ch9vjbv.oh8tk.us-east-2.rds.amazonaws.com',
+    'database': 'SideRideSchemaTest',
+    'raise_on_warnings': True
+}
+
+@pytest.mark.parametrize("ride", [
     {
-        'user': 'SideRideProjectxyz',
-        'password': 'SideRideProject130*',
-        'host': 'database-side-ride-project.ch9vjbvoh8tk.us-east-2.rds.amazonaws.com',
-        'database': 'SideRideSchema',
-        'raise_on_warnings': True
+        'driver': 'Tom Cruise', 
+        'from': 'Scientology Building',
+        'to': 'Cedars Sinai Medical Center',
+        'fromLat': 34.098091,
+        'fromLng': -118.294243, 
+        'toLat': 34.074940, 
+        'toLng': -118.382420, 
+        'make': 'Subaru', 
+        'plate': '7XTD345', 
+        'datetime': '2021-11-25 11:11:11', 
+        'price': 17, 
+        'seats': 3, 
+        'model': 'Outback'  
     }, 
-
-    #password is incorrect, expect fail
     {
-        'user': 'SideRideProject',
-        'password': 'wrongpassword',
-        'host': 'database-side-ride-project.ch9vjbvoh8tk.us-east-2.rds.amazonaws.com',
-        'database': 'SideRideSchema',
-        'raise_on_warnings': True
-    }, 
+        'driver': 'Red Riding Hood', 
+        'from': 'Angeles National Forest',
+        'to': 'Grandmas Deli Babushka',
+        'fromLat': 34.291540,
+        'fromLng': -118.362560, 
+        'toLat': 34.097470, 
+        'toLng': -118.349980, 
+        'make': 'Harley Davidson', 
+        'plate': 'R3DH00D', 
+        'datetime': '2021-11-24 22:11:11', 
+        'price': 5, 
+        'seats': 2, 
+        'model': 'Heritage'  
+    }
+])
 
-    #host is incorrect, expect fail
+@pytest.mark.parametrize("rider", [
     {
-        'user': 'SideRideProject',
-        'password': 'SideRideProject130*',
-        'host': 'database-side-ride-project.ch9vjbvoh8tk.us-east-2.rds.amazonafdsafasdfws.com',
-        'database': 'SideRideSchema',
-        'raise_on_warnings': True
+        'id': 1,
+        'username': 'tony'
+    },
+        {
+        'id': 2,
+        'username': 'obama'
     }, 
+        {
+        'id': 3,
+        'username': 'bush'
+    },  
+])
 
-    #db is incorrect, expect fail
+@pytest.mark.parametrize("searchParams", [
     {
-        'user': 'SideRideProject',
-        'password': 'SideRideProject130*',
-        'host': 'database-side-ride-project.ch9vjbvoh8tk.us-east-2.rds.amazonaws.com',
-        'database': 'SideRide',
-        'raise_on_warnings': True
+        'fromLat': 34.098091,
+        'fromLng': -118.294243,
+        'toLat': 34.074940,
+        'toLng': -118.382420, 
+        'date': "2021-11-24",
     }, 
-]
-
-
-class TestDBConnection(unittest.TestCase):
-
-    def test_connect_failures(self):
-        for config in test_configs:
-            sql_db = DatabaseHandler(config)
-            with self.subTest(config):
-                self.assertEqual(sql_db.connect_to_db(), CONN_FAILURE)
+        {
+        'fromLat': 39.098091,
+        'fromLng': 118.294243,
+        'toLat': 34.074940,
+        'toLng': -118.382420, 
+        'date': "2021-11-24",
+    } 
+])
+#expect pass
+def test_DB_pipeline(ride, rider, searchParams):
+    errors = []
+    db = dm.Database(default_config)
     
-    def test_connect_proper(self):
-        sql_db = DatabaseHandler()
-        self.assertNotEqual(sql_db.connect_to_db(), CONN_FAILURE)
-    
-    def test_add_ride(self):
-        # First properly connect to DB and grab the handle 
-        sql_db = DatabaseHandler()
-        sql_db.connect_to_db()
+    if db.connect_to_db() == "Connection failed":
+        errors.append("Connnection Failed")
+    else: 
+        #test create ride
+        if db.create_ride(rm.Ride(ride)) == False:
+            errors.append("Create Ride Failed")
 
-        # Now try inserting a row and check response from DatabaseHandler 
-        # TODO 
+        if db.add_driver(rm.Ride(ride)) != True: 
+            errors.append("Add driver failed" + str(ride))
+        
+        if db.add_rider(rider['id'], rider['username']) != (0, True):
+            errors.append("Add rider failed" + str(rider))
+        
+        # if db.update_seatCount(ride) != True:
+        #     errors.append("Update Seat Count failed" + str(ride))
 
 
-if __name__ == '__main__':
-    unittest.main()
-    
+        #if find rides doenst return a list, it returned err
+        if type(db.find_rides(searchParams)) != list:
+            errors.append("Find rides failed" + str(db.find_rides(searchParams)))
+
+        #if my rides doesnt return a list, it returned err
+        if type(db.myrides(ride['driver'])) != dict:
+            errors.append("Myrides failed" + str(db.myrides(ride['driver'])))
+
+        if db.delete_riders() != True:
+            errors.append("Delete riders failed")
+
+        if db.cleanup_rides() != True:
+             errors.append("Cleanup rides failed" + str(ride))        
+
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
